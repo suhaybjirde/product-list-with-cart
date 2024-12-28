741315f860ecd4a31e80a0ab5f4f24521fd9d4d9
suhaybjirde <dribblekayga@gmail.com>
2024-12-28 18:11:22 +0300

    remove cancel btn from model cartItem

--- src/component/CartItem.jsx
+++ src/component/CartItem.jsx
@@ -17,12 +17,15 @@ const CartItem = ({
             <p className="flex gap-3 ">
                 <span className="text-red font-semibold">{productCount}x</span> 
                 <span className="text-rose-400">@ ${productPrice.toFixed(2)}</span>
-                <span className="text-rose-500">${(productCount * productPrice).toFixed(2)}</span>
+                {!dialog && (<span className="text-rose-500">${(productCount * productPrice).toFixed(2)}</span>)}
             </p>
         </div>
-        <button className="group transition-all hover:outline-rose-900 outline outline-1 outline-rose-400 p-1 ml-auto rounded-full p" onClick={()=> dispatch({type: ACTIONS.REMOVEITEM, payload: {id: productId}})}>
-            <svg className="group-hover:fill-rose-900 transition-all ease-in" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="hsl(7, 20%, 60%)" viewBox="0 0 10 10"><path d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
-        </button>
+        {!dialog && (
+            <button className="group transition-all hover:outline-rose-900 outline outline-1 outline-rose-400 p-1 ml-auto rounded-full p" onClick={()=> dispatch({type: ACTIONS.REMOVEITEM, payload: {id: productId}})}>
+                <svg className="group-hover:fill-rose-900 transition-all ease-in" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="hsl(7, 20%, 60%)" viewBox="0 0 10 10"><path d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
+            </button>
+        )}
+        {dialog && (<span className="text-rose-900 font-semibold ml-auto">${(productCount * productPrice).toFixed(2)}</span>)}
     </div>
 )
 
